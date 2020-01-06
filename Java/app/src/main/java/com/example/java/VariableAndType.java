d95bc293d0438c803faf15503ed31c74e7559ce8
areong9292 <areong9292@gmail.com>
2020-01-06 22:54:11 +0900

    [Lecture - Android Basic with Java] 20. Variable And Type

---
+++ Java/app/src/main/java/com/example/java/VariableAndType.java
@@ -0,0 +1,41 @@
+package com.example.java;
+
+public class VariableAndType {
+
+    public static void main(String[] args) {
+        short number1;
+        int number2;
+        long number3;
+
+        float number4;
+        double number5;
+
+        boolean truefalse;
+
+        char writing;
+
+        number1 = 10;
+        number2 = 9;
+        number3 = 8;
+
+        number4 = 1.1f;
+        number5 = 2.2;
+
+        truefalse = true;
+        truefalse = false;
+
+        writing = '일';
+
+        int a;
+        int b;
+
+        a = 1 + 2 + 3 + 4 + 5;
+        b = 6 + 7 + 8 + 9;
+
+        int result;
+        result = a + b;
+
+        // 1 ~ 9까지 합 출력
+        System.out.println(result);
+    }
+}
