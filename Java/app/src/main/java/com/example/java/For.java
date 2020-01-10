8ca71ff63ead9caf7c93ee33dd82cef7324c755b
areong9292 <areong9292@gmail.com>
2020-01-11 00:08:24 +0900

    [Lecture - Android Basic with Java] 28. While and For

---
+++ Java/app/src/main/java/com/example/java/For.java
@@ -0,0 +1,18 @@
+package com.example.java;
+
+public class For {
+    public static void main(String[] args)
+    {
+        /*for (int i = 0; i < 10; i++)
+        {
+            System.out.println("돈까스를 한입 먹는다");
+        }*/
+
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("돈까스를 한입 먹는다");
+            if(i == 5)
+                break;
+        }
+    }
+}
--- Java/app/src/main/java/com/example/java/While.java
+++ Java/app/src/main/java/com/example/java/While.java
@@ -1,4 +1,32 @@
 package com.example.java;
 
 public class While {
+    public static void main1(String[] args)
+    {
+        boolean hungry = true;
+
+        /*while(hungry == true)
+        {
+            System.out.println("돈까스를 한입 먹는다");
+        }*/
+
+        /*while(hungry == true)
+        {
+            System.out.println("돈까스를 한입 먹는다");
+            System.out.println("돈까스를 한입 먹는다");
+            System.out.println("돈까스를 한입 먹는다");
+            System.out.println("돈까스를 한입 먹는다");
+            break;
+        }*/
+
+        /*while(hungry == true)
+        {
+            System.out.println("돈까스를 한입 먹는다");
+            System.out.println("돈까스를 한입 먹는다");
+            System.out.println("돈까스를 한입 먹는다");
+            System.out.println("돈까스를 한입 먹는다");
+            hungry = false;
+            continue;
+        }*/
+    }
 }
