4421b5113370865af7843a065e282e08e833c3f5
areong9292 <areong9292@gmail.com>
2020-01-23 20:37:53 +0900

    [Lecture - Android Basic with Java] 56. Intent - 2

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -17,9 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("LifeCycle", "onCreate");
 
+        /// 명시적 인텐트
         // 다른 액티비티 실행
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+
+        // 액티비티에 데이터를 보낸다
+        intent.putExtra("INTENT_KEY", 5);
+
+        intent.putExtra("INTENT_KEY_STRING", "STRING");
+        // startActivity(intent); - 암시적 때문에 주석함
+
+        /// 암시적 인텐트
+        Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"));
+        startActivity(intent1);
     }
 
     @Override
--- MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -11,6 +12,21 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // 호출한 곳에서 보낸 데이터를 받는다
+        Intent intent = getIntent();
+
+        // 보낸 타입을 보고 맞는 메소드, 자료형 쓰면 됨
+        // String data = intent.getStringExtra("INTENT_KEY");
+
+        // int형은 이런 식으로 하면 INTENT_KEY를 int로 가져오는데 실패하면 0으로 가져올 수 있게 지정 가능
+        int data = intent.getIntExtra("INTENT_KEY", 0);
+
+        // string은 그런 거 없다
+        String stringData = intent.getStringExtra("INTENT_KEY_STRING");
+
+        Log.d("intent_key", "" + data);
+        Log.d("intent_key_string", "" + stringData);
     }
 
     @Override
