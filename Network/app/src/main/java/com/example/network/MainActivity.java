0bdd55e95b66462fb9d6aa69df5e78f57d3dd424
areong9292 <areong9292@gmail.com>
2020-03-04 20:01:56 +0900

    [Lecture - Android Basic with Java] 94. Network - 2

--- Network/app/src/main/java/com/example/network/MainActivity.java
+++ Network/app/src/main/java/com/example/network/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.network;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.google.gson.JsonObject;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -14,39 +19,91 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Stetho Debugging address
+    // chrome://inspect/#devices
+
+    TextView textView1;
+    TextView textView2;
+    MyService service;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 네트워크 디버깅 라이브러리
+        // Retrofit에 끼워넣어서 오고가는 패킷을 확인할 수 있다
+        // 스테토 초기화
+        Stetho.initializeWithDefaults(this);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addNetworkInterceptor(new StethoInterceptor()).build();
+
         // 안드로이드 네트워크 라이브러리
         // Retrofit 객체 생성
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        MyService service = retrofit.create(MyService.class);
+        service = retrofit.create(MyService.class);
 
+        // 텍스트뷰 클릭 때 통신하게 수정
+        textView1 = findViewById(R.id.textview1);
+        textView1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                requestRepo();
+            }
+        });
+
+        // 텍스트뷰 클릭 때 통신하게 수정
+        textView2 = findViewById(R.id.textview2);
+        textView2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                postUser();
+            }
+        });
+    }
+
+    public void postUser()
+    {
+        Call<JsonObject> postUser = service.postUser("areong9292", 20);
+        postUser.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void requestRepo()
+    {
         // 인터페이스에서 지정한 타입과 똑같이 해야함
         // 통신 전송 준비 완료
         Call<JsonObject> requestUserRepo = service.getUseRepositories("areong9292");
 
         // 통신 전송
         // enqueue로 통신 시 비동기로 진행 됨
-        requestUserRepo.enqueue(new Callback<JsonObject>()
-        {
+        requestUserRepo.enqueue(new Callback<JsonObject>() {
             // 응답 성공
             @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response)
-            {
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
 
             }
 
             // 응답 실패
             @Override
-            public void onFailure(Call<JsonObject> call, Throwable t)
-            {
+            public void onFailure(Call<JsonObject> call, Throwable t) {
 
             }
         });
--- Network/app/src/main/java/com/example/network/MyService.java
+++ Network/app/src/main/java/com/example/network/MyService.java
@@ -4,7 +4,10 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface MyService {
@@ -18,4 +21,13 @@ public interface MyService {
     // {user에 어떤 게 들어가는지 지정}
     // getUseRepositories 사용 시 스트링 값을 인자로 보내면 된다
     Call<JsonObject> getUseRepositories(@Path("user")String userName);
+
+
+    // 데이터를 실어서 보내는 방법
+    @FormUrlEncoded
+    @POST("users/areong9292/repos")
+
+    // username이라는 필드에 name 스트링 값을 넣는다
+    // age라는 필드에 age int 값을 넣는다
+    Call<JsonObject> postUser(@Field("username") String name, @Field("age") int age);
 }
